7e2f6146afa7f27949edaa0122dc760361efcd66
manvominh <vominhman@gmail.com>
2023-11-01 14:41:18 +0700

    revert navbar.js

---
+++ Blogger.WebUI/wwwroot/js/navbar.js
@@ -0,0 +1,8 @@
+﻿var a = document.querySelectorAll(".site-menu li a");
+for (var i = 0, length = a.length; i < length; i++) {
+	a[i].onclick = function () {
+		var b = document.querySelector(".site-menu li.active");
+		if (b) b.classList.remove("active");
+		this.parentNode.classList.add('active');
+	};
+}
