b590020bab518469edb24e7d6adbc5d5f1d92266
manvominh <vominhman@gmail.com>
2023-11-01 11:46:30 +0700

    make button Comment disable/enable when have value by IJSRuntime

---
+++ Blogger.WebUI/wwwroot/js/comment.js
@@ -0,0 +1,12 @@
+﻿function MakeAddCommentButtonVisible() {
+    var edValue = document.getElementById("commentText");
+    var s = edValue.value;
+    if (s.length === 0) {
+        document.getElementById("btnAddComment").setAttribute("disabled", "true");
+    } else {
+        document.getElementById("btnAddComment").removeAttribute("disabled");
+    }
+}
+function DisableAddCommentButton() {
+    document.getElementById("btnAddComment").setAttribute("disabled", "true");
+}
