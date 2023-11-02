cd1b85e200d8611306b8ee53fc1eea2bd6015d19
manvominh <vominhman@gmail.com>
2023-11-02 10:25:00 +0700

    modify the way to call navbar.js on NavMenu component on OnAfterRenderAsync event

--- Blogger.WebUI/wwwroot/js/navbar.js
+++ Blogger.WebUI/wwwroot/js/navbar.js
@@ -1,8 +1,10 @@
-﻿var a = document.querySelectorAll(".site-menu li a");
-for (var i = 0, length = a.length; i < length; i++) {
-	a[i].onclick = function () {
-		var b = document.querySelector(".site-menu li.active");
-		if (b) b.classList.remove("active");
-		this.parentNode.classList.add('active');
-	};
+﻿function ActiveMenu() {
+	var a = document.querySelectorAll(".site-menu li a");
+	for (var i = 0, length = a.length; i < length; i++) {
+		a[i].onclick = function () {
+			var b = document.querySelector(".site-menu li.active");
+			if (b) b.classList.remove("active");
+			this.parentNode.classList.add('active');
+		};
+	}
 }
